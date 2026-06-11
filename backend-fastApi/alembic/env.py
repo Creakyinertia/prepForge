ce677713d05f8430b881e84fe416f93cc3b9f304
Darshan Reddy <dareddy@estuate.com>
2026-06-11 11:54:22 +0530

    migration scipt

--- backend-fastApi/alembic/env.py
+++ backend-fastApi/alembic/env.py
@@ -13,6 +13,7 @@ from models.topic import Topic
 from models.roadmap_topic import RoadmapTopic
 from models.revision import Revision
 from models.note import Note
+from models.resource import Resource
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
